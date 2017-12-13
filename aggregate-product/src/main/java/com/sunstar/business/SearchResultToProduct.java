baf0f67622306b97006592c30d4a156741deebd8
peter <2464375449@qq.com>
2017-12-13 19:14:13 +0800

    修改

--- aggregate-product/src/main/java/com/sunstar/business/SearchResultToProduct.java
+++ aggregate-product/src/main/java/com/sunstar/business/SearchResultToProduct.java
@@ -12,10 +12,12 @@ public class SearchResultToProduct {
     private static final Logger logger = Logger.getLogger(SearchResultToProduct.class);
 
 
-
     public static long importData( ){
-        String url = "jdbc:mysql://192.168.0.183:3306/couponsdb?user=root&password=Sunstar123!&useUnicode=true&characterEncoding=UTF-8";
-        String sql = "SELECT a.classid,a.spname,a.sppic,0,0,1,b.value_id,a.id FROM ss_hj_search_result a LEFT JOIN ss_hj_value b ON (a.brand_name = b.value_name AND b.quantity_id = 1) WHERE a.id not in (select autoid from ss_hj_product_relation)";
+        String url = "jdbc:mysql://192.168.0.183:3306/couponsdb?user=root&password=" +
+                "Sunstar123!&useUnicode=true&characterEncoding=UTF-8&useCursorFetch=true";
+        String sql = "SELECT a.classid,a.spname,a.sppic,0,0,1,b.value_id,a.id FROM " +
+                "ss_hj_search_result a LEFT JOIN ss_hj_value b ON (a.brand_name = b.value_name " +
+                "AND b.quantity_id = 1) WHERE a.id not in (select autoid from ss_hj_product_relation)";
         try {
             Class.forName("com.mysql.jdbc.Driver");
         } catch (ClassNotFoundException e1) {
@@ -23,7 +25,6 @@ public class SearchResultToProduct {
         }
         long allStart = System.currentTimeMillis();
         long count =0;
-        long count_no_valueId =0;
         Connection con = null;
         Connection con1 = null;
         PreparedStatement psHJSearchResult = null;
@@ -35,30 +36,29 @@ public class SearchResultToProduct {
         int pictureId=1711669;
         int relationId=252381;
         try {
+            //查询操作
             con = DriverManager.getConnection(url);
-            con1 = DriverManager.getConnection(url);
-            // Set auto-commit to false
             con.setAutoCommit(false);
-            con1.setAutoCommit(false);
             //查询ss_hj_search_result合格数据
-            psHJSearchResult = con.prepareStatement(sql,ResultSet.TYPE_FORWARD_ONLY);
-            psHJSearchResult.setFetchSize(Integer.MIN_VALUE);
-            psHJSearchResult.setFetchDirection(ResultSet.FETCH_REVERSE);
-            //插入ss_hj_product
-            psHJProduct = con1.prepareStatement("insert into ss_hj_product(PRODUCT_ID,CLS_ID,PRODUCT_NAME,MAIN_PICTURE,SALES_VOLUME,COMMENTS_NUMBER,STS,PRIORITY,BRAND_ID) values(?,?,?,?,?,?,?,?,?)");
-            //插入ss_hj_product_picture
-            psHJProductPicture = con1.prepareStatement("insert into ss_hj_product_picture(PRODUCT_ID,PRODUCT_URL,PRODUCT_VIEW_URL,PICTURE_ID) values(?,?,?,?)");
-            //插入ss_hj_product_relation
-            psHJProductRelation = con1.prepareStatement("insert into ss_hj_product_relation(PRODUCT_ID,AUTOID,RELATION_ID) values(?,?,?)");
+            psHJSearchResult = con.prepareStatement(sql,ResultSet.TYPE_FORWARD_ONLY,
+                    ResultSet.CONCUR_READ_ONLY);
+            psHJSearchResult.setFetchSize(1);
+            //psHJSearchResult.setFetchDirection(ResultSet.FETCH_REVERSE);
             rs = psHJSearchResult.executeQuery();
+            // 写入操作
+            con1 = DriverManager.getConnection(url);
+            con1.setAutoCommit(false);
+            //插入ss_hj_product
+            psHJProduct = con1.prepareStatement("insert into ss_hj_product" +
+                    "(PRODUCT_ID,CLS_ID,PRODUCT_NAME,MAIN_PICTURE,SALES_VOLUME," +
+                    "COMMENTS_NUMBER,STS,PRIORITY,BRAND_ID) values(?,?,?,?,?,?,?,?,?)");
+            //插入ss_hj_product_picture
+            psHJProductPicture = con1.prepareStatement("insert into ss_hj_product_picture" +
+                    "(PRODUCT_ID,PRODUCT_URL,PRODUCT_VIEW_URL,PICTURE_ID) values(?,?,?,?)");
+            //插入ss_hj_product_relation
+            psHJProductRelation = con1.prepareStatement("insert into ss_hj_product_relation" +
+                    "(PRODUCT_ID,AUTOID,RELATION_ID) values(?,?,?)");
             while (rs.next()) {
-                //此处处理业务逻辑
-
-                    if(rs.getInt("value_id")==0){
-                        logger.error("valueId品牌为空,autoid:"+rs.getInt("id"));
-                        count_no_valueId++;
-                        //continue;//品牌为空不导入
-                    }
                     //聚合商品
                     psHJProduct.setInt(1,productId);
                     psHJProduct.setInt(2,rs.getInt("classid"));
@@ -81,12 +81,6 @@ public class SearchResultToProduct {
                     psHJProductRelation.setInt(2,rs.getInt("id"));
                     psHJProductRelation.setInt(3,relationId);
                     psHJProductRelation.addBatch();
-                    //聚合商品
-                    logger.info("insert into ss_hj_product(PRODUCT_ID,CLS_ID,PRODUCT_NAME,MAIN_PICTURE,SALES_VOLUME,COMMENTS_NUMBER,STS,PRIORITY,BRAND_ID) values("+productId+","+rs.getInt("classid")+",'"+rs.getString("spname").replaceAll("'","’")+"','"+rs.getString("sppic")+"',0,0,1,"+productId+","+rs.getInt("value_id")+");");
-                    //图片
-                    logger.info("insert into ss_hj_product_picture(PRODUCT_ID,PRODUCT_URL,PRODUCT_VIEW_URL,PICTURE_ID) values("+productId+",'"+rs.getString("sppic")+"','"+rs.getString("sppic")+"',"+pictureId+");");
-                    //关系表
-                    logger.info("insert into ss_hj_product_relation(PRODUCT_ID,AUTOID,RELATION_ID) values("+productId+","+rs.getInt("id")+","+relationId+");");
                     //id增加
                     productId++;
                     pictureId++;
@@ -104,16 +98,13 @@ public class SearchResultToProduct {
             psHJProductPicture.executeBatch();
             psHJProductRelation.executeBatch();
             con1.commit();
-        } catch (SQLException e) {
-            logger.error("productId:"+productId);
-            logger.error("pictureId:"+pictureId);
-            logger.error("relationId:"+relationId);
+        } catch (Exception e) {
+            logger.error("异常");
             e.printStackTrace();
         } finally {
             logger.error("=productId:"+productId);
             logger.error("=pictureId:"+pictureId);
             logger.error("=relationId:"+relationId);
-            logger.error("count_no_valueId:"+count_no_valueId);
             try {
                 if(rs!=null){
                     rs.close();
@@ -137,7 +128,6 @@ public class SearchResultToProduct {
             }
         }
         return count;
-
     }
 
     public static void main(String[] args) throws InterruptedException {
